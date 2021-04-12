9d533a6fec751f3206ea66e27b5af6224b3ebe0f
Gabriela <gabrielapatru12@gmail.com>
2021-04-12 09:34:40 +0300

    A si B

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_1/colocviu1_1MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_1/colocviu1_1MainActivity.java
@@ -1,8 +1,12 @@
 package ro.pub.cs.systems.eim.Colocviu1_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.provider.SyncStateContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,14 +14,16 @@ import android.widget.TextView;
 
 public class colocviu1_1MainActivity extends AppCompatActivity {
 
-    private Button north, east, west, south;
+    private Button north, east, west, south, second;
     private TextView textView1;
+    int i=0;
 
     private NorthClickListener northClickListener = new NorthClickListener();
 
     private class NorthClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
+            i++;
             textView1.setText(textView1.getText().toString() +
                     ((Button)north).getText().toString() + ", ");
         }
@@ -27,6 +33,7 @@ public class colocviu1_1MainActivity extends AppCompatActivity {
     private class SouthClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
+            i++;
             textView1.setText(textView1.getText().toString() +
                     ((Button)south).getText().toString() + ", ");
         }
@@ -37,6 +44,7 @@ public class colocviu1_1MainActivity extends AppCompatActivity {
     private class EastClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
+            i++;
             textView1.setText(textView1.getText().toString() +
                     ((Button)east).getText().toString() + ", ");
         }
@@ -47,14 +55,32 @@ public class colocviu1_1MainActivity extends AppCompatActivity {
     private class WestClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
+            i++;
             textView1.setText(textView1.getText().toString() +
                     ((Button)west).getText().toString() + ", ");
+        }
+    }
+
+    private secondClickListener secondClickListener = new secondClickListener();
+
+    private class secondClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+           textView1.setText(textView1.getText().toString() +
+                     i + ", ");
         }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colocviu1_1_main);
+
+        if (savedInstanceState != null){
+            if (savedInstanceState.containsKey("textView1") ){
+                TextView usernameEditText = (TextView)findViewById(R.id.textView);
+                usernameEditText.setText(savedInstanceState.getString("textView1"));
+            }
+        }
 
         north = (Button)findViewById(R.id.north);
         north.setOnClickListener(northClickListener);
@@ -65,5 +91,20 @@ public class colocviu1_1MainActivity extends AppCompatActivity {
         south = (Button)findViewById(R.id.south);
         south.setOnClickListener(southClickListener);
         textView1 = (TextView) findViewById(R.id.textView);
+
+        second = (Button)findViewById(R.id.second);
+        second.setOnClickListener(secondClickListener);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        TextView usernameEditText = (TextView) findViewById(R.id.textView);
+        savedInstanceState.putString("textView1", usernameEditText.getText().toString());
+        if (savedInstanceState.containsKey("textView1")) {
+            TextView text = (TextView)findViewById(R.id.textView);
+            usernameEditText.setText(savedInstanceState.getString("textView1"));
+        }
     }
 }
